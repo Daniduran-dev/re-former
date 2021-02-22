01285c64de47f1c9b8eceb84d8bc003254206932
Daniduran-dev <danigduran@gmail.com>
2021-02-22 23:01:31 +0700

    add controller Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 50 }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 50 }
+  validates :password, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 50 }
 end
