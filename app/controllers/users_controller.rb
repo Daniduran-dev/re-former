8f5fcdadaea69636d61c8141be59963df9d50172
Daniduran-dev <danigduran@gmail.com>
2021-02-22 23:16:55 +0700

    create view new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = 'Object successfully created'
+      redirect_to @user
+    else
+      flash[:error] = 'Something went wrong'
+      render 'new'
+    end
+  end
 end
